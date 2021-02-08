a0c32ba9e238177e221ef3970aba1c3acdf7d9fb
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2021-02-08 11:07:12 +0530

    Adding display and length function in the linkedlist

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -8,9 +8,32 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+//to display the singly the linked list
+    public void display() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    //to find the length of singly linked list
+    public int length(){
+        if(head==null){
+            return 0;}
+        int count = 0;
+        ListNode current = head;
+        while (current!=null){
 
+           count++;
+           current= current.next;
+
+        }
+        return count;
+    }
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
+        //head is instance variable
         sll.head = new ListNode(10);
         ListNode second = new ListNode(1);
         ListNode third = new ListNode(8);
@@ -20,6 +43,10 @@ public class SinglyLinkedList {
         sll.head.next = second;
         second.next = third;
         third.next = fourth;
+
+        sll.display();
+        int m = sll.length();
+        System.out.println("length of singly linked list is " + m);
 
     }
 
