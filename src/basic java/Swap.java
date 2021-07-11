107030e6a3ced752c8804404a9f29654fffa8b03
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2021-07-11 16:07:08 +0530

    swapping without 3rd variable

---
+++ src/basic java/Swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+    public static void main(String args[]){
+        int a=1,b=2;
+        a= a+b;
+        b= a-b;
+        a = a-b;
+
+        System.out.print( a + " and " + b);
+
+    }
+}
